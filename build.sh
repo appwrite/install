b935897bbee8a17bbf54748540adb57a33688696
Eldad Fux <eldad.fux@gmail.com>
2020-03-18 18:05:52 +0200

    Updated build

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+echo "Updating git repository $1 / $2"
+
+git fetch origin
+git reset --hard origin/master
+
 echo 'Starting build...'
+
+composer update --ignore-platform-reqs --optimize-autoloader --no-plugins --no-scripts --prefer-dist
 
 docker build --tag appwrite/install:"$1" .
 
